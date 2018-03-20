68fca39daa289f27a8789c4c818f79e6cd70ed3b
Tushar Mathur <tusharmath@gmail.com>
2018-03-20 17:10:24 +0530

    add basic js animations

--- src/app.js
+++ src/app.js
@@ -1,26 +1,41 @@
 import { h } from './dom.js'
 
-const seconds = R.compose(O.scan(R.inc, 0), O.interval)
-const sec1000$ = seconds(1000)
-const sec100$ = seconds(100)
-const together$ = O.sample(R.identity, O.interval(5000), [
-  O.combine((a, b) => `${Math.min(a, 15)}-${Math.min(b, 100)}`, [
-    sec1000$,
-    sec100$
-  ])
-])
+const seconds = R.compose(
+  O.multicast,
+  O.scan(i => (i === 9 ? 0 : i + 1), 0),
+  O.interval
+)
+const MAX_RANGE = 100
+const getINC = (n, i) =>
+  (n === MAX_RANGE && i > 0) || (n === 0 && i < 0) ? -i : i
 
-const view = e =>
-  h('div', {}, [
-    h('p', { on: { click: e.of('p').emit } }, ['AAAA']),
-    h('div', { on: { click: e.of('div').emit } }, ['every 1000ms:', sec1000$]),
-    h('div', { on: { click: e.of('div').emit } }, [
-      'Pretty updated:',
-      together$
-    ]),
-    h('h1', { on: { click: e.of('h1').emit } }, ['every 100ms:', sec100$])
-  ])
+const translate = R.compose(
+  O.multicast,
+  O.slice(0, 20000),
+  O.map(R.compose(R.divide(R.__, MAX_RANGE), R.nth(0))),
+  O.scan(([n, i]) => [n + getINC(n, i), getINC(n, i)], [0, 1]),
+  O.frames
+)
+
+const translate$ = translate()
+
+const transform = cnt => i => ({
+  transform: `translateX(${cnt * i * 100}px)`
+})
+const timer$ = seconds(1000)
+const Dot = cnt => {
+  const style = O.map(transform(cnt), translate$)
+  return h('div.element', { style }, [timer$])
+}
+
+const Triangle = () => {
+  return h('div.triangle', R.times(Dot, 10))
+}
+
+const view = e => h('div', [Triangle()])
 
 const emitter = hoe.create(console.log)
 
 O.forEach(el => document.body.appendChild(el), view(emitter))
+
+// O.forEach(console.log, O.slice(0, 20, translate$))
