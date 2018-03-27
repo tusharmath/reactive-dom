d03680374915db0694b97838b89912391acbedea
Tushar Mathur <tusharmath@gmail.com>
2018-03-27 09:43:37 +0530

    refactor(test): combine mulitple tests into one group

--- test/HTMLElementObservable.test.ts
+++ test/HTMLElementObservable.test.ts
@@ -7,6 +7,154 @@ import {EventStart} from 'observable-air/src/internal/Events'
 
 const node = (results: any[]) => (results[0] ? results[0].value : null)
 describe('HTMLElementObservable', () => {
+  describe('children', () => {
+    it('should attach child HTMLElement', () => {
+      const sh = createTestScheduler()
+      const {results} = sh.start(
+        () => new HTMLElementObservable('div.a', {}, [O.of('A'), O.of('B')])
+      )
+      const expected = [
+        EVENT.next(
+          201,
+          html(`<div class="a"><span>A</span><span>B</span></div>`)
+        ),
+        EVENT.complete(201)
+      ]
+      assert.deepEqual(results, expected)
+    })
+
+    it('should maintain child order', () => {
+      const sh = createTestScheduler()
+      const {results} = sh.subscribeTo(
+        () =>
+          new HTMLElementObservable('div.a', {}, [
+            sh.Hot('---A|'),
+            sh.Hot('--B---|')
+          ])
+      )
+      sh.advanceTo(201)
+      assert.deepEqual(node(results), null)
+      sh.advanceTo(202)
+      assert.deepEqual(
+        node(results),
+        html(`<div class="a"><span>B</span></div>`)
+      )
+      sh.advanceTo(203)
+      assert.deepEqual(
+        node(results),
+        html(`<div class="a"><span>A</span><span>B</span></div>`)
+      )
+      sh.advanceTo(2000)
+      assert.deepEqual(results, [
+        EVENT.next(
+          202,
+          html(`<div class="a"><span>A</span><span>B</span></div>`)
+        ),
+        EVENT.complete(206)
+      ])
+    })
+
+    it('should wait for children before inserting into dom', () => {
+      const sh = createTestScheduler()
+      const {results} = sh.start(
+        () => new HTMLElementObservable('div.a', {}, [sh.Hot('-----A----|')])
+      )
+      const expected = [
+        EVENT.next(205, html(`<div class="a"><span>A</span></div>`)),
+        EVENT.complete(210)
+      ]
+      assert.deepEqual(results, expected)
+    })
+
+    it('should update child nodes with time', () => {
+      const sh = createTestScheduler()
+      const {results} = sh.subscribeTo(
+        () => new HTMLElementObservable('div.a', {}, [sh.Hot('--ABC|')])
+      )
+      sh.advanceTo(201)
+      assert.deepEqual(node(results), null)
+      sh.advanceTo(202)
+      assert.deepEqual(
+        node(results),
+        html(`<div class="a"><span>A</span></div>`)
+      )
+      sh.advanceTo(203)
+      assert.deepEqual(
+        node(results),
+        html(`<div class="a"><span>B</span></div>`)
+      )
+      sh.advanceTo(204)
+      assert.deepEqual(
+        node(results),
+        html(`<div class="a"><span>C</span></div>`)
+      )
+
+      sh.advanceTo(205)
+      assert.deepEqual(results, [
+        EVENT.next(202, html(`<div class="a"><span>C</span></div>`)),
+        EVENT.complete(205)
+      ])
+    })
+
+    it('should update text without create a new span', () => {
+      const sh = createTestScheduler()
+      const {results} = sh.subscribeTo(
+        () => new HTMLElementObservable('div.a', {}, [sh.Hot('--AB|')])
+      )
+      sh.advanceTo(202)
+      const div210 = node(results)
+      const span210 = div210.childNodes[0]
+      assert.deepEqual(div210, html(`<div class="a"><span>A</span></div>`))
+
+      sh.advanceTo(203)
+      const div220 = node(results)
+      const span220 = div220.childNodes[0]
+      assert.deepEqual(div220, html(`<div class="a"><span>B</span></div>`))
+
+      assert.strictEqual(div210, div220)
+      assert.strictEqual(span210, span220)
+    })
+    it('should create elements with empty string', () => {
+      const sh = createTestScheduler()
+      const {results} = sh.start(
+        () => new HTMLElementObservable('div.wonky', {}, [O.of('')])
+      )
+      const expected = [
+        EVENT.next(201, html(`<div class="wonky"></div>`)),
+        EVENT.complete(201)
+      ]
+      assert.deepEqual(results, expected)
+    })
+
+    it('should should delete elements on empty string', () => {
+      const sh = createTestScheduler()
+      const child$ = sh.Hot(
+        EVENT.next(212, html(`<h1><span>Air</span></h1>`)),
+        EVENT.next(213, ``),
+        EVENT.next(214, html(`<h1><span>No Air</span></h1>`)),
+        EVENT.complete(215)
+      )
+      const {results} = sh.subscribeTo(
+        () => new HTMLElementObservable('div.wonky', {}, [child$])
+      )
+
+      assert.deepEqual(node(results), null)
+      sh.advanceTo(212)
+      assert.deepEqual(
+        node(results),
+        html(`<div class="wonky"><h1><span>Air</span></h1></div>`)
+      )
+
+      sh.advanceTo(213)
+      assert.deepEqual(node(results), html(`<div class="wonky"></div>`))
+
+      sh.advanceTo(214)
+      assert.deepEqual(
+        node(results),
+        html(`<div class="wonky"><h1><span>No Air</span></h1></div>`)
+      )
+    })
+  })
   it('should create a new HTMLElement', () => {
     const sh = createTestScheduler()
     const {results} = sh.start(
@@ -17,101 +165,6 @@ describe('HTMLElementObservable', () => {
       EVENT.complete(201)
     ]
     assert.deepEqual(results, expected)
-  })
-
-  it('should attach child HTMLElement', () => {
-    const sh = createTestScheduler()
-    const {results} = sh.start(
-      () => new HTMLElementObservable('div.a', {}, [O.of('A'), O.of('B')])
-    )
-    const expected = [
-      EVENT.next(
-        201,
-        html(`<div class="a"><span>A</span><span>B</span></div>`)
-      ),
-      EVENT.complete(201)
-    ]
-    assert.deepEqual(results, expected)
-  })
-
-  it('should maintain child order', () => {
-    const sh = createTestScheduler()
-    const {results} = sh.subscribeTo(
-      () =>
-        new HTMLElementObservable('div.a', {}, [
-          sh.Hot('---A|'),
-          sh.Hot('--B---|')
-        ])
-    )
-    sh.advanceTo(201)
-    assert.deepEqual(node(results), null)
-    sh.advanceTo(202)
-    assert.deepEqual(node(results), html(`<div class="a"><span>B</span></div>`))
-    sh.advanceTo(203)
-    assert.deepEqual(
-      node(results),
-      html(`<div class="a"><span>A</span><span>B</span></div>`)
-    )
-    sh.advanceTo(2000)
-    assert.deepEqual(results, [
-      EVENT.next(
-        202,
-        html(`<div class="a"><span>A</span><span>B</span></div>`)
-      ),
-      EVENT.complete(206)
-    ])
-  })
-
-  it('should wait for children before inserting into dom', () => {
-    const sh = createTestScheduler()
-    const {results} = sh.start(
-      () => new HTMLElementObservable('div.a', {}, [sh.Hot('-----A----|')])
-    )
-    const expected = [
-      EVENT.next(205, html(`<div class="a"><span>A</span></div>`)),
-      EVENT.complete(210)
-    ]
-    assert.deepEqual(results, expected)
-  })
-
-  it('should update child nodes with time', () => {
-    const sh = createTestScheduler()
-    const {results} = sh.subscribeTo(
-      () => new HTMLElementObservable('div.a', {}, [sh.Hot('--ABC|')])
-    )
-    sh.advanceTo(201)
-    assert.deepEqual(node(results), null)
-    sh.advanceTo(202)
-    assert.deepEqual(node(results), html(`<div class="a"><span>A</span></div>`))
-    sh.advanceTo(203)
-    assert.deepEqual(node(results), html(`<div class="a"><span>B</span></div>`))
-    sh.advanceTo(204)
-    assert.deepEqual(node(results), html(`<div class="a"><span>C</span></div>`))
-
-    sh.advanceTo(205)
-    assert.deepEqual(results, [
-      EVENT.next(202, html(`<div class="a"><span>C</span></div>`)),
-      EVENT.complete(205)
-    ])
-  })
-
-  it('should update text without create a new span', () => {
-    const sh = createTestScheduler()
-    const {results} = sh.subscribeTo(
-      () => new HTMLElementObservable('div.a', {}, [sh.Hot('--AB|')])
-    )
-    sh.advanceTo(202)
-    const div210 = node(results)
-    const span210 = div210.childNodes[0]
-    assert.deepEqual(div210, html(`<div class="a"><span>A</span></div>`))
-
-    sh.advanceTo(203)
-    const div220 = node(results)
-    const span220 = div220.childNodes[0]
-    assert.deepEqual(div220, html(`<div class="a"><span>B</span></div>`))
-
-    assert.strictEqual(div210, div220)
-    assert.strictEqual(span210, span220)
   })
 
   describe('style$', () => {
@@ -189,46 +242,6 @@ describe('HTMLElementObservable', () => {
       ]
       assert.deepEqual(actual, expected)
     })
-  })
-  it('should create elements with empty string', () => {
-    const sh = createTestScheduler()
-    const {results} = sh.start(
-      () => new HTMLElementObservable('div.wonky', {}, [O.of('')])
-    )
-    const expected = [
-      EVENT.next(201, html(`<div class="wonky"></div>`)),
-      EVENT.complete(201)
-    ]
-    assert.deepEqual(results, expected)
-  })
-
-  it('should should delete elements on empty string', () => {
-    const sh = createTestScheduler()
-    const child$ = sh.Hot(
-      EVENT.next(212, html(`<h1><span>Air</span></h1>`)),
-      EVENT.next(213, ``),
-      EVENT.next(214, html(`<h1><span>No Air</span></h1>`)),
-      EVENT.complete(215)
-    )
-    const {results} = sh.subscribeTo(
-      () => new HTMLElementObservable('div.wonky', {}, [child$])
-    )
-
-    assert.deepEqual(node(results), null)
-    sh.advanceTo(212)
-    assert.deepEqual(
-      node(results),
-      html(`<div class="wonky"><h1><span>Air</span></h1></div>`)
-    )
-
-    sh.advanceTo(213)
-    assert.deepEqual(node(results), html(`<div class="wonky"></div>`))
-
-    sh.advanceTo(214)
-    assert.deepEqual(
-      node(results),
-      html(`<div class="wonky"><h1><span>No Air</span></h1></div>`)
-    )
   })
 
   describe('props$', () => {
