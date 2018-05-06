da9e34e6b5de065c642477c80aceb203cfde8b30
Tushar Mathur <tusharmath@gmail.com>
2018-05-06 10:21:47 +0530

    refactor(demo): update demo

--- demo/app.ts
+++ demo/app.ts
@@ -7,84 +7,101 @@
 
 import * as O from 'observable-air'
 import {IObservable} from 'observable-air'
-import {h, hStatic} from '../index'
+import {h} from '../index'
 
+/**
+ * Real world data in an immutable format
+ */
 type Input = {
-  keyPress$: IObservable<KeyboardEvent>
+  inputText$: IObservable<string>
   storage$: IObservable<Array<string>>
 }
 
+/**
+ * Represents the application state
+ */
 type State = {
   todo$: IObservable<Array<string>>
   inputProps$: IObservable<any>
-  footerStyle$: IObservable<any>
 }
 
+/**
+ * Takes in the real world mutable data and returns streams of immutable type
+ * @param {Document} document
+ * @returns {Input}
+ */
 const input = (document: Document): Input => {
-  const event$ = <T extends Event>(i: string) => O.multicast<T>(O.fromDOM(document as any, i))
-  const keyPress$ = O.filter(i => i.key === 'Enter', event$<KeyboardEvent>('keypress'))
-  const OLD_DATA = localStorage.getItem('DATA')
-  const storage$ = O.tap(console.log, O.of(OLD_DATA ? JSON.parse(OLD_DATA) : []))
-  return {keyPress$, storage$}
+  const inputText$ = O.multicast(
+    O.map(
+      (e: any) => e.target.value,
+      O.filter<KeyboardEvent>(i => i.key === 'Enter', O.fromDOM(document as any, 'keypress'))
+    )
+  )
+  const storage$ = new O.Observable(ob => {
+    const OLD_DATA = localStorage.getItem('DATA')
+    if (OLD_DATA) ob.next(JSON.parse(OLD_DATA))
+    else ob.next([])
+    ob.complete()
+  })
+
+  return {inputText$, storage$}
 }
 
-const update = ({keyPress$, storage$}: Input): State => {
+/**
+ * Takes in real world immutable data and converts it into application state
+ * @param {IObservable<string>} inputText$
+ * @param {IObservable<Array<string>>} storage$
+ * @returns {State}
+ */
+const update = ({inputText$, storage$}: Input): State => {
   const INPUT_PROPS = {placeholder: 'What need to be done?', autofocus: true, value: ''}
+  const text$ = O.filter(_ => _ !== '', inputText$)
+  const todo$ = O.merge(O.flatMap(todo => O.scan((data, i) => [i, ...data], todo, text$), storage$), storage$)
+  const inputProps$ = O.concat(O.of(INPUT_PROPS), O.mapTo(INPUT_PROPS, text$))
+  return {todo$, inputProps$}
+}
+
+/**
+ * Converts the state into an actual functioning DOM tree
+ * @param {IObservable<Array<string>>} todo$
+ * @param {IObservable<any>} inputProps$
+ * @returns {hReturnType}
+ */
+const view = ({todo$, inputProps$}: State) => {
   const DISPLAY_NONE = {display: 'none'}
-
-  const enter$ = O.filter(_ => _ !== '', O.map((_: any) => _.target.value, keyPress$))
-  const todo$ = O.merge(O.flatMap(data => O.scan((data, i) => [i, ...data], data, enter$), storage$), storage$)
-  const inputProps$ = O.concat(O.of(INPUT_PROPS), O.mapTo(INPUT_PROPS, enter$))
-  const footerStyle$ = O.concat(O.of(DISPLAY_NONE), O.map(_ => (_.length > 0 ? {display: ''} : DISPLAY_NONE), todo$))
-  return {todo$, inputProps$, footerStyle$}
-}
-
-const view = ({todo$, inputProps$, footerStyle$}: State) => {
-  return h('div', [
-    h('section.todoapp', [
-      h('header.header', [
-        h('h1', ['todos']),
-        h('input.new-todo', {
-          props: inputProps$
-        })
-      ]),
-      h('section.main', [
-        h('input.toggle-all', {props: {type: 'checkbox'}}),
-        h('label.toggle-all', ['Mark all as complete']),
-        h('div', [O.map(_ => _.join(':'), todo$)]),
-        O.switchMap(
-          todo =>
-            h(
-              'ul.todo-list',
-              todo.map(_ =>
-                h('li', [
-                  h('input.toggle', {props: {type: 'checkbox'}}),
-                  h('label', [_]),
-                  h('button.destroy', [hStatic('')])
-                ])
-              )
-            ),
-          todo$
-        )
-      ]),
-      h('footer.footer', {style: footerStyle$}, [
-        h('span.todo-count', [O.map(_ => `${_.length} items left`, todo$)]),
-        h('div.filters', [h('a.selected', ['All']), h('a', ['Active']), h('a', ['Completed'])]),
-        h('button.clear-completed', ['Clear Completed'])
-      ])
-    ]),
-    h('footer.info', [
-      h('p', ['Double-click to edit a todo']),
-      h('p', ['Written by', h('a', {props: {href: 'https://twitter.com/tusharmath'}}, ['Tushar Mathur'])]),
-      h('p', ['Part of', h('a', {props: {href: 'http://todomvc.com'}}, ['TodoMVC'])])
-    ])
+  const footerStyle$ = O.map(_ => (_.length > 0 ? {display: ''} : DISPLAY_NONE), todo$)
+  const ListItem = _ =>
+    h('li', [h('input.toggle', {props: {type: 'checkbox'}}), h('label', [_]), h('button.destroy', [''])])
+  const Footer = h('footer.info', [
+    h('p', ['Double-click to edit a todo']),
+    h('p', ['Written by', h('a', {props: {href: 'https://twitter.com/tusharmath'}}, ['Tushar Mathur'])]),
+    h('p', ['Part of', h('a', {props: {href: 'http://todomvc.com'}}, ['TodoMVC'])])
   ])
+  const TodoHeader = h('header.header', [
+    h('h1', ['todos']),
+    h('input.new-todo', {
+      props: inputProps$
+    })
+  ])
+  const TodoFooter = h('footer.footer', {style: footerStyle$}, [
+    h('span.todo-count', [O.map(_ => `${_.length} items left`, todo$)]),
+    h('div.filters', [h('a.selected', ['All']), h('a', ['Active']), h('a', ['Completed'])]),
+    h('button.clear-completed', ['Clear Completed'])
+  ])
+  const TodoList = h('section.main', [
+    h('input.toggle-all', {props: {type: 'checkbox'}}),
+    h('label.toggle-all', ['Mark all as complete']),
+    O.switchMap(todo => {
+      return h('ul.todo-list', todo.map(ListItem))
+    }, todo$)
+  ])
+  return h('div', [h('section.todoapp', [TodoHeader, TodoList, TodoFooter]), Footer])
 }
+
+const state = update(input(document))
 
 document.addEventListener('DOMContentLoaded', () => {
-  O.forEach(i => document.body.appendChild(i), view(update(input(document))))
+  O.forEach(i => document.body.appendChild(i), view(state))
 })
 
-O.forEach(data => localStorage.setItem('DATA', JSON.stringify(data)), update(input(document)).todo$)
-
-// O.forEach(console.log, update(input(document)).todo$)
+O.forEach(data => localStorage.setItem('DATA', JSON.stringify(data)), state.todo$)
