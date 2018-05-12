a31378e7412986995e018ced0cd515b12605f592
Tushar Mathur <tusharmath@gmail.com>
2018-05-12 18:52:46 +0530

    refactor(rd-element): rd-element public api only allows VNode to be used for updating itself

--- src/internal/RDElement.ts
+++ src/internal/RDElement.ts
@@ -13,58 +13,34 @@ import {RDAttributes, RDEventListeners, RDProps, RDStyles, VNode} from './VNode'
 export class RDElement {
   private elm?: HTMLElement
   private set = new RDSet()
-  private elmMap = new Map<number, Node>()
-  private style?: any
-  private attrs?: any
+  private elmMap = new Map<number, RDElement>()
+  private style?: RDStyles
+  private attrs?: RDAttributes
   private on?: RDEventListeners
+  private sel?: string
 
-  getElm() {
-    if (this.elm) return this.elm
-    throw new Error('Element has not be initialized')
-  }
-
-  init(sel: string) {
-    if (this.elm) throw new Error('Element already initialized')
-    this.elm = createElement(sel)
-  }
-
-  addAt(elm: Node, id: number) {
-    const child = elm
-    if (this.set.has(id)) this.getElm().replaceChild(child, this.getElm().childNodes[id])
-    else if (Number.isFinite(this.set.gte(id)))
-      this.getElm().insertBefore(child, this.elmMap.get(this.set.gte(id)) as Node)
-    else this.getElm().appendChild(child)
-    this.set = this.set.add(id)
-    this.elmMap.set(id, child)
-  }
-
-  removeAt(id: number) {
-    const node = this.elmMap.get(id)
-    if (node) this.getElm().removeChild(node)
-  }
-
-  setAttrs(attrs: RDAttributes) {
+  private setAttrs(attrs: RDAttributes) {
     const {add, del} = objectDiff(attrs, this.attrs)
     del.forEach(_ => this.getElm().removeAttribute(_))
     add.forEach(_ => this.getElm().setAttribute(_, attrs[_]))
     this.attrs = attrs
   }
 
-  setStyle(style: RDStyles) {
+  private setStyle(style: RDStyles) {
     const {add, del} = objectDiff(style, this.style)
     del.forEach(_ => this.getElm().style.removeProperty(_))
     add.forEach(_ => this.getElm().style.setProperty(_, (style as any)[_]))
     this.style = style
   }
 
-  setProps(props: RDProps) {
+  private setProps(props: RDProps) {
     const elm = this.getElm() as any
     const {add, del} = objectDiff(props, elm)
     del.forEach(_ => delete elm[_])
     add.forEach(_ => (elm[_] = props[_]))
   }
 
-  setListeners(on: RDEventListeners) {
+  private setListeners(on: RDEventListeners) {
     const {add, del} = objectDiff(on, this.on)
     del.forEach(_ => {
       if (this.on) return this.getElm().removeEventListener(_, this.on[_])
@@ -74,11 +50,61 @@ export class RDElement {
     this.on = on
   }
 
+  private getChildRDElm(node: VNode, id: number): RDElement {
+    return this.elmMap.has(id) && (this.elmMap.get(id) as RDElement).sel === node.sel
+      ? (this.elmMap.get(id) as RDElement)
+      : new RDElement(node)
+  }
+  private init(sel: string) {
+    if (this.sel === sel) return
+    if (this.elm) throw new Error('Element already initialized')
+    this.elm = createElement(sel)
+    this.sel = sel
+  }
+
+  constructor(node: VNode) {
+    this.patch(node)
+  }
+  getElm() {
+    if (this.elm) return this.elm
+    throw new Error('Element has not be initialized')
+  }
+
+  addAt(node: VNode, id: number) {
+    const rd = this.getChildRDElm(node, id)
+    const child = rd.getElm()
+
+    if (this.elmMap.has(id) && (this.elmMap.get(id) as RDElement).sel !== node.sel) {
+      const oldRDElement = this.elmMap.get(id) as RDElement
+      this.getElm().replaceChild(child, oldRDElement.getElm())
+      oldRDElement.setListeners({})
+    } else if (this.elmMap.has(id) && (this.elmMap.get(id) as RDElement).sel === node.sel) {
+      const child = this.elmMap.get(id) as RDElement
+      child.patch(node)
+    } else if (Number.isFinite(this.set.gte(id))) {
+      const referenceNode = this.elmMap.get(this.set.gte(id)) as RDElement
+      this.getElm().insertBefore(child, referenceNode.getElm())
+    } else {
+      this.getElm().appendChild(child)
+    }
+    this.set = this.set.add(id)
+    this.elmMap.set(id, rd)
+  }
+
+  removeAt(id: number) {
+    const node = this.elmMap.get(id)
+    if (node) {
+      this.getElm().removeChild(node.getElm())
+      node.setListeners({})
+    }
+  }
+
   patch(node: VNode) {
-    if (!this.elm) this.init(node.sel)
+    this.init(node.sel)
     if (node.attrs) this.setAttrs(node.attrs)
     if (node.props) this.setProps(node.props)
     if (node.style) this.setStyle(node.style)
     if (node.on) this.setListeners(node.on)
+    return this
   }
 }
--- test/internal/RDElement.test.ts
+++ test/internal/RDElement.test.ts
@@ -8,8 +8,7 @@ import {RDElement} from '../../src/internal/RDElement'
 describe('RDElement', () => {
   describe('patch', () => {
     it('should update attributes', () => {
-      const elm = new RDElement()
-      elm.patch({
+      const elm = new RDElement({
         sel: 'div.container',
         attrs: {
           data: 'carbon-di-oxide'
@@ -19,8 +18,7 @@ describe('RDElement', () => {
       assert.equal(actual, `<div class="container" data="carbon-di-oxide"></div>`)
     })
     it('should update props', () => {
-      const elm = new RDElement()
-      elm.patch({
+      const elm = new RDElement({
         sel: 'div.container',
         props: {
           id: 'carbon-di-oxide'
@@ -28,10 +26,8 @@ describe('RDElement', () => {
       })
       assert.equal(elm.getElm().id, 'carbon-di-oxide')
     })
-
     it('should update styles', () => {
-      const elm = new RDElement()
-      elm.patch({
+      const elm = new RDElement({
         sel: 'div.container',
         style: {
           color: 'red'
@@ -40,12 +36,10 @@ describe('RDElement', () => {
       const actual = elm.getElm().outerHTML
       assert.equal(actual, `<div class="container" style="color: red;"></div>`)
     })
-
     it('should add event listeners', () => {
       let count = 0
       const onClick = () => count++
-      const elm = new RDElement()
-      elm.patch({
+      const elm = new RDElement({
         sel: 'div.container',
         on: {
           click: onClick
@@ -54,14 +48,104 @@ describe('RDElement', () => {
       elm.getElm().dispatchEvent(new Event('click'))
       assert.equal(count, 1, 'No event listeners were attached')
     })
-  })
-  describe('init()', () => {
+
     context('already initialized', () => {
-      it('should throw', () => {
-        const elm = new RDElement()
-        elm.init('div.whoopy')
-        assert.throws(() => elm.init('div.whoopie'), 'Element already initialized')
+      context('and same selector', () => {
+        it('should not throw', () => {
+          const elm = new RDElement({sel: 'div.whoopy'})
+          assert.doesNotThrow(() => elm.patch({sel: 'div.whoopy'}))
+        })
       })
+      context('diff selector', () => {
+        it('should throw', () => {
+          const elm = new RDElement({sel: 'div.container'})
+          assert.throws(() => elm.patch({sel: 'div.container-2'}), 'Element already initialized')
+        })
+      })
+    })
+  })
+  describe('addAt()', () => {
+    it('should append child', () => {
+      const rd = new RDElement({sel: 'ul'})
+      rd.addAt({sel: 'li'}, 0)
+      const actual = rd.getElm().outerHTML
+      const expected = `<ul><li></li></ul>`
+      assert.equal(actual, expected)
+    })
+
+    it('should maintain order', () => {
+      const rd = new RDElement({sel: 'ul'})
+      rd.addAt({sel: 'li.__7'}, 7)
+      rd.addAt({sel: 'li.__1'}, 1)
+      const actual = rd.getElm().outerHTML
+      const expected = `<ul><li class="__1"></li><li class="__7"></li></ul>`
+      assert.equal(actual, expected)
+    })
+
+    context('index is same', () => {
+      it('should apply the diff', () => {
+        const rd = new RDElement({sel: 'ul'})
+        rd.addAt({sel: 'li', style: {color: 'red'}}, 7)
+        rd.addAt({sel: 'li', style: {color: 'green'}}, 7)
+        const actual = rd.getElm().outerHTML
+        const expected = `<ul><li style="color: green;"></li></ul>`
+        assert.equal(actual, expected)
+      })
+
+      it('should not create a new element', () => {
+        const rd = new RDElement({sel: 'ul'})
+        rd.addAt({sel: 'li', style: {color: 'red'}}, 7)
+        const node0 = rd.getElm().childNodes[0]
+        rd.addAt({sel: 'li', style: {color: 'green'}}, 7)
+        const node1 = rd.getElm().childNodes[0]
+        assert.strictEqual(node0, node1)
+      })
+
+      context('selector is diff', () => {
+        it('should create a new child', () => {
+          const rd = new RDElement({sel: 'ul'})
+          rd.addAt({sel: 'li.aaa'}, 7)
+          rd.addAt({sel: 'li.bbb'}, 7)
+          const actual = rd.getElm().outerHTML
+          const expected = `<ul><li class="bbb"></li></ul>`
+          assert.equal(actual, expected)
+        })
+
+        it('should remove event listeners', () => {
+          let count = 0
+          const rd = new RDElement({sel: 'ul'})
+          const onClick = () => count++
+          rd.addAt({sel: 'li.aaa', on: {click: onClick}}, 7)
+          const node = rd.getElm().childNodes[0]
+          rd.addAt({sel: 'li.bbb'}, 7)
+          node.dispatchEvent(new Event('click'))
+          assert.equal(count, 0)
+        })
+      })
+    })
+  })
+  describe('removeAt()', () => {
+    it('should remove dom node', () => {
+      const rd = new RDElement({sel: 'ul'})
+      rd.addAt({sel: 'li.__7'}, 7)
+      rd.addAt({sel: 'li.__1'}, 1)
+      rd.addAt({sel: 'li.__3'}, 3)
+      rd.removeAt(1)
+      const actual = rd.getElm().outerHTML
+      const expected = `<ul><li class="__3"></li><li class="__7"></li></ul>`
+      assert.equal(actual, expected)
+    })
+    it('should remove listeners from the removed node', () => {
+      let count = 0
+      const onClick = () => count++
+      const rd = new RDElement({sel: 'ul'})
+      rd.addAt({sel: 'li.__7', on: {click: onClick}}, 7)
+      rd.addAt({sel: 'li.__1', on: {click: onClick}}, 1)
+      rd.addAt({sel: 'li.__3', on: {click: onClick}}, 3)
+      const node = rd.getElm().childNodes[0]
+      rd.removeAt(1)
+      node.dispatchEvent(new Event('click'))
+      assert.equal(count, 0)
     })
   })
 })
