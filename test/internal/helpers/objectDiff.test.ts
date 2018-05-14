59edda6c5c660cd3b709dfd181157c3837332ba7
Tushar Mathur <tusharmath@gmail.com>
2018-05-14 08:11:06 +0530

    refactor(internal): add more tests for object diff

--- test/internal/helpers/objectDiff.test.ts
+++ test/internal/helpers/objectDiff.test.ts
@@ -6,10 +6,18 @@ import {assert} from 'chai'
 import {objectDiff} from '../../../src/internal/helpers/objectDiff'
 
 describe('objectDiff', () => {
-  it('should return common', () => {
+  it('should return add/del/com', () => {
     const {add, com, del} = objectDiff(new Set(['a', 'c']), new Set(['a', 'b']))
     assert.deepEqual(com, ['a'])
     assert.deepEqual(del, ['b'])
     assert.deepEqual(add, ['c'])
+  })
+
+  it('should skip if ref is same', () => {
+    const set = new Set(['A', 'B'])
+    const {add, com, del} = objectDiff(set, set)
+    assert.deepEqual(com, ['A', 'B'])
+    assert.deepEqual(del, [])
+    assert.deepEqual(add, [])
   })
 })
