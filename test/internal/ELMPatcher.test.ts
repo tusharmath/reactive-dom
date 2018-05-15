8c5ebebcc1022ab00bf5dcbca33cd95c04f59a48
Tushar Mathur <tusharmath@gmail.com>
2018-05-15 07:00:37 +0530

    refactor(test): restructure the test cases

--- test/internal/ELMPatcher.test.ts
+++ test/internal/ELMPatcher.test.ts
@@ -7,103 +7,6 @@ import {ELMPatcher} from '../../src/internal/ELMPatcher'
 
 describe('ELMPatcher', () => {
   describe('patch', () => {
-    it('should update attributes', () => {
-      const elm = new ELMPatcher({
-        sel: 'div.container',
-        attrs: {
-          data: 'carbon-di-oxide'
-        }
-      })
-      const actual = elm.getElm().outerHTML
-      assert.equal(
-        actual,
-        `<div class="container" data="carbon-di-oxide"></div>`
-      )
-    })
-    it('should update props', () => {
-      const elm = new ELMPatcher({
-        sel: 'div.container',
-        props: {
-          id: 'carbon-di-oxide'
-        }
-      })
-      assert.equal(elm.getElm().id, 'carbon-di-oxide')
-    })
-    it('should update styles', () => {
-      const elm = new ELMPatcher({
-        sel: 'div.container',
-        style: {
-          color: 'red'
-        }
-      })
-      const actual = elm.getElm().outerHTML
-      assert.equal(actual, `<div class="container" style="color: red;"></div>`)
-    })
-    it('should add event listeners', () => {
-      let count = 0
-      const onClick = () => count++
-      const elm = new ELMPatcher({
-        sel: 'div.container',
-        on: {
-          click: onClick
-        }
-      })
-      elm.getElm().dispatchEvent(new Event('click'))
-      assert.equal(count, 1, 'No event listeners were attached')
-    })
-    it('should update children', () => {
-      const elm = new ELMPatcher({
-        sel: 'ul.nav',
-        children: [{sel: 'li'}, {sel: 'li.active'}, {sel: 'li'}]
-      })
-      const actual = elm.getElm().outerHTML
-      const expected = `<ul class="nav"><li></li><li class="active"></li><li></li></ul>`
-      assert.equal(actual, expected)
-    })
-    it('should append child', () => {
-      const rd = new ELMPatcher({sel: 'ul'})
-      rd.patch({sel: 'ul', children: [{sel: 'li'}]})
-      const actual = rd.getElm().outerHTML
-      const expected = `<ul><li></li></ul>`
-      assert.equal(actual, expected)
-    })
-    it('should maintain order', () => {
-      const rd = new ELMPatcher({sel: 'ul'})
-      rd.patch({sel: 'ul', children: [{sel: 'li.__7'}]})
-      rd.patch({sel: 'ul', children: [{sel: 'li.__1'}, {sel: 'li.__7'}]})
-      const actual = rd.getElm().outerHTML
-      const expected = `<ul><li class="__1"></li><li class="__7"></li></ul>`
-      assert.equal(actual, expected)
-    })
-    it('should remove dom node', () => {
-      const rd = new ELMPatcher({sel: 'ul'})
-      rd.patch({sel: 'ul', children: [{sel: 'li.__7'}]})
-      rd.patch({sel: 'ul', children: [{sel: 'li.__1'}, {sel: 'li.__7'}]})
-      rd.patch({
-        sel: 'ul',
-        children: [{sel: 'li.__1'}, {sel: 'li.__3'}, {sel: 'li.__7'}]
-      })
-      rd.patch({sel: 'ul', children: [{sel: 'li.__3'}, {sel: 'li.__7'}]})
-      const actual = rd.getElm().outerHTML
-      const expected = `<ul><li class="__3"></li><li class="__7"></li></ul>`
-      assert.equal(actual, expected)
-    })
-    it('should remove listeners from the removed node', () => {
-      let count = 0
-      const onClick = () => count++
-      const rd = new ELMPatcher({sel: 'ul'})
-      rd.patch({
-        sel: 'ul',
-        children: [
-          {sel: 'li.__1', on: {click: onClick}},
-          {sel: 'li.__2', on: {click: onClick}}
-        ]
-      })
-      const node = rd.getElm().childNodes[1]
-      rd.patch({sel: 'ul', children: [{sel: 'li.__1', on: {click: onClick}}]})
-      node.dispatchEvent(new Event('click'))
-      assert.equal(count, 0)
-    })
     context('already initialized', () => {
       context('and same selector', () => {
         it('should not throw', () => {
@@ -165,8 +68,51 @@ describe('ELMPatcher', () => {
         })
       })
     })
-
+    describe('on', () => {
+      it('should add event listeners', () => {
+        let count = 0
+        const onClick = () => count++
+        const elm = new ELMPatcher({
+          sel: 'div.container',
+          on: {
+            click: onClick
+          }
+        })
+        elm.getElm().dispatchEvent(new Event('click'))
+        assert.equal(count, 1, 'No event listeners were attached')
+      })
+      it('should remove listeners from the removed node', () => {
+        let count = 0
+        const onClick = () => count++
+        const rd = new ELMPatcher({sel: 'ul'})
+        rd.patch({
+          sel: 'ul',
+          children: [
+            {sel: 'li.__1', on: {click: onClick}},
+            {sel: 'li.__2', on: {click: onClick}}
+          ]
+        })
+        const node = rd.getElm().childNodes[1]
+        rd.patch({sel: 'ul', children: [{sel: 'li.__1', on: {click: onClick}}]})
+        node.dispatchEvent(new Event('click'))
+        assert.equal(count, 0)
+      })
+    })
     describe('style', () => {
+      it('should update styles', () => {
+        const elm = new ELMPatcher({
+          sel: 'div.container',
+          style: {
+            color: 'red'
+          }
+        })
+        const actual = elm.getElm().outerHTML
+        assert.equal(
+          actual,
+          `<div class="container" style="color: red;"></div>`
+        )
+      })
+
       context('style key is removed', () => {
         it('should remove the styles from dom elm', () => {
           const elm = new ELMPatcher({sel: 'div.whoopy', style: {color: 'red'}})
@@ -178,6 +124,20 @@ describe('ELMPatcher', () => {
       })
     })
     describe('attrs', () => {
+      it('should update attributes', () => {
+        const elm = new ELMPatcher({
+          sel: 'div.container',
+          attrs: {
+            data: 'carbon-di-oxide'
+          }
+        })
+        const actual = elm.getElm().outerHTML
+        assert.equal(
+          actual,
+          `<div class="container" data="carbon-di-oxide"></div>`
+        )
+      })
+
       context('attrs key is removed', () => {
         it('should remove the attrs from dom elm', () => {
           const elm = new ELMPatcher({sel: 'a', attrs: {href: '/a'}})
@@ -189,6 +149,16 @@ describe('ELMPatcher', () => {
       })
     })
     describe('props', () => {
+      it('should update props', () => {
+        const elm = new ELMPatcher({
+          sel: 'div.container',
+          props: {
+            id: 'carbon-di-oxide'
+          }
+        })
+        assert.equal(elm.getElm().id, 'carbon-di-oxide')
+      })
+
       context('props key is removed', () => {
         it('should remove the props from dom elm', () => {
           const elm: any = new ELMPatcher({sel: 'h1', props: {asd: 'ALPHA'}})
@@ -197,6 +167,46 @@ describe('ELMPatcher', () => {
           const actual = elm.getElm().asd
           assert.isUndefined(actual)
         })
+      })
+    })
+    describe('children', () => {
+      it('should update children', () => {
+        const elm = new ELMPatcher({
+          sel: 'ul.nav',
+          children: [{sel: 'li'}, {sel: 'li.active'}, {sel: 'li'}]
+        })
+        const actual = elm.getElm().outerHTML
+        const expected = `<ul class="nav"><li></li><li class="active"></li><li></li></ul>`
+        assert.equal(actual, expected)
+      })
+      it('should append child', () => {
+        const rd = new ELMPatcher({sel: 'ul'})
+        rd.patch({sel: 'ul', children: [{sel: 'li.__0', key: '0'}]})
+        rd.patch({sel: 'ul', children: [{sel: 'li.__0', key: '0'}, {sel: 'li.__1', key: '1'}]})
+        const actual = rd.getElm().outerHTML
+        const expected = `<ul><li class="__0"></li><li class="__1"></li></ul>`
+        assert.equal(actual, expected)
+      })
+      it('should maintain order', () => {
+        const rd = new ELMPatcher({sel: 'ul'})
+        rd.patch({sel: 'ul', children: [{sel: 'li.__7'}]})
+        rd.patch({sel: 'ul', children: [{sel: 'li.__1'}, {sel: 'li.__7'}]})
+        const actual = rd.getElm().outerHTML
+        const expected = `<ul><li class="__1"></li><li class="__7"></li></ul>`
+        assert.equal(actual, expected)
+      })
+      it('should remove dom node', () => {
+        const rd = new ELMPatcher({sel: 'ul'})
+        rd.patch({sel: 'ul', children: [{sel: 'li.__7'}]})
+        rd.patch({sel: 'ul', children: [{sel: 'li.__1'}, {sel: 'li.__7'}]})
+        rd.patch({
+          sel: 'ul',
+          children: [{sel: 'li.__1'}, {sel: 'li.__3'}, {sel: 'li.__7'}]
+        })
+        rd.patch({sel: 'ul', children: [{sel: 'li.__3'}, {sel: 'li.__7'}]})
+        const actual = rd.getElm().outerHTML
+        const expected = `<ul><li class="__3"></li><li class="__7"></li></ul>`
+        assert.equal(actual, expected)
       })
     })
   })
